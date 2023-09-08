5f88db356706d469cde71e13d75a385ce1357043
Joonseo Yoon <141890165+121jsy@users.noreply.github.com>
2023-09-08 13:42:07 +0900

    Update 2_Game_Statistics.py
    Updated to V2

--- pages/2_Game_Statistics.py
+++ pages/2_Game_Statistics.py
@@ -3,7 +3,7 @@ from Dashboard import *
 import plotly.express as px 
 import streamlit as st
 import pandas as pd
-import datetime
+from PIL import Image
 
 
 # list of indicators to display in each excel sheets "User Traffic" and "Game Statistics"
@@ -14,15 +14,12 @@ GAME_STATISTICS_INDIC = ['날짜', '스퀴즈볼 총 획득량\n(순수 획득)'
                         'NCPP\n인당 채팅 참여', 'IUPP\n인당 아이템 사용']
 
 
-st.set_page_config(layout="wide")
-st.title("GAME STATISTICS")
-
-df_ut, df_gs = load_from_file()
-
 def display_table(df, indicator):
     show_table = st.checkbox("Display Table")
     if show_table:
-        st.dataframe(df[['날짜', indicator]], height=700)
+        # display '날짜' and chosen indicator in a table
+        # .iloc[::1] to reverse the order of DataFrame        
+        st.dataframe((df.iloc[::-1])[['날짜', indicator]], height=700)
 
 def game_statistics_draw(df, indicator):
     with col2_1:
@@ -30,26 +27,32 @@ def game_statistics_draw(df, indicator):
     with col2_2:
         display_table(df, indicator)
 
+im = Image.open("squizrun_png.png")
+st.set_page_config(page_title="Squizrun Analytics", page_icon=im, layout="wide")
+st.title("GAME STATISTICS")
+
+df_ut, df_gs = load_from_file()
 
 # Four columns for dynamic dropdown menu and date range (col1_4 and col1_5 are placeholders for indentation)
 col1_1, col1_2, col1_3, col1_4 = st.columns([0.3, 0.45, 0.17, 0.08])
-
-
-with col1_3:
-    filtered_df = date_range(df_gs)
 
 # First column in the first row for indicator dropdown menu
 with col1_1:
     option = st.selectbox(
         '지표 선택',
         ('-', '인당 스퀴즈볼 총 획득량', '인당 스퀴즈볼 사용량', '총 퀴즈 참여자 수', '인당 퀴즈 참여 (NQPP)', '인당 채팅 참여 (NCPP)', '인당 아이템 사용 (IUPP)'))
-    
+
+with col1_3:
+    filtered_df = date_range(df_gs)
 
 # Two columns in the second row for the graph and table
 data_container = st.container()
 with data_container:
     col2_1, col2_2 = st.columns((3,1))
 
+# Display DAU, MAU, WAU as default
+if option == '-':
+    game_statistics_draw(filtered_df, '총 퀴즈 참여자 수')
 
 if option == '인당 스퀴즈볼 총 획득량':
     game_statistics_draw(filtered_df, '스퀴즈볼 총 획득량\n(순수 획득)')
@@ -68,4 +71,3 @@ elif option == '인당 채팅 참여 (NCPP)':
 
 elif option == '인당 아이템 사용 (IUPP)':
     game_statistics_draw(filtered_df, 'IUPP\n인당 아이템 사용')
-
